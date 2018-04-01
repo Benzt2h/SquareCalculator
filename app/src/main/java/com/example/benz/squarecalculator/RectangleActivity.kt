929fb843325b8b317166f57e31b8da5c0dddf22e
benzt2h <benzt2h.work@gmail.com>
2018-04-02 03:36:51 +0700

    .

--- app/src/main/java/com/example/benz/squarecalculator/RectangleActivity.kt
+++ app/src/main/java/com/example/benz/squarecalculator/RectangleActivity.kt
@@ -1,21 +1,21 @@
-package com.example.benz.squarecalculator
+    package com.example.benz.squarecalculator
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_rectangle.*
+    import android.support.v7.app.AppCompatActivity
+    import android.os.Bundle
+    import android.widget.Toast
+    import kotlinx.android.synthetic.main.activity_rectangle.*
 
-class RectangleActivity : AppCompatActivity() {
+    class RectangleActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rectangle)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_rectangle)
 
-        btnSum.setOnClickListener({
-            if(edHeigth.text.toString()!=""&&edWidth.text.toString()!=""){
-                tvSum.text=(edHeigth.text.toString().toDouble()*edWidth.text.toString().toDouble()).toString()+" ซม."
-                Toast.makeText(this,"คำตอบเท่ากับ ${tvSum.text}", Toast.LENGTH_LONG).show()
-            }else Toast.makeText(this,"ใส่ค่าที่ต้องการคำนวณก่อน", Toast.LENGTH_LONG).show()
-        })
+            btnSum.setOnClickListener({
+                if(edHeigth.text.toString()!=""&&edWidth.text.toString()!=""){
+                    tvSum.text=(edHeigth.text.toString().toDouble()*edWidth.text.toString().toDouble()).toString()+" ซม."
+                    Toast.makeText(this,"คำตอบเท่ากับ ${tvSum.text}", Toast.LENGTH_LONG).show()
+                }else Toast.makeText(this,"ใส่ค่าที่ต้องการคำนวณก่อน", Toast.LENGTH_LONG).show()
+            })
+        }
     }
-}
